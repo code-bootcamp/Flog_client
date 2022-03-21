7c1edfe5184b26e964e7215278980cc58ac42a7b
Jooeun-K <90050960+Jooeun-K@users.noreply.github.com>
2022-03-21 19:02:26 +0900

    Design: [#7] 레이아웃 컴포넌트 UI 완료

--- flog/src/components/commons/layout/header/LayoutHeader.container.tsx
+++ flog/src/components/commons/layout/header/LayoutHeader.container.tsx
@@ -1,5 +1,20 @@
+import { useState } from "react";
 import LayoutHeaderUI from "./LayoutHeader.presenter";
 
 export default function LayoutHeader() {
-  return <LayoutHeaderUI />;
+  const [isActive, setIsActive] = useState(false);
+  const onClickLogo = () => {
+    alert("Logo 클릭 시 작동하는 함수");
+  };
+  const onClickHamburgerBtn = () => {
+    setIsActive((prev) => !prev);
+  };
+
+  return (
+    <LayoutHeaderUI
+      onClickLogo={onClickLogo}
+      onClickHamburgerBtn={onClickHamburgerBtn}
+      isActive={isActive}
+    />
+  );
 }
--- flog/src/components/commons/layout/header/LayoutHeader.presenter.tsx
+++ flog/src/components/commons/layout/header/LayoutHeader.presenter.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import { HAMBURGER_MENUS, NAVIGATION_MENUS } from "../Menus";
 import * as Header from "./LayoutHeader.styles";
+import { ILayoutHeaderUIProps } from "./LayoutHeader.types";
 
-export default function LayoutHeaderUI() {
+export default function LayoutHeaderUI(props: ILayoutHeaderUIProps) {
   return (
     <Header.Container>
       <Header.InnerWrap>
-        <Header.Logo>
+        <Header.Logo onClick={props.onClickLogo}>
           <h1>Flog</h1>
         </Header.Logo>
         <Header.MenuBar>
@@ -22,23 +23,28 @@ export default function LayoutHeaderUI() {
             </ul>
           </Header.Navigation>
           <Header.Hamburger>
-            <Header.HamburgerBtn>
+            <Header.HamburgerBtn onClick={props.onClickHamburgerBtn}>
               <Header.HamburgerBtnLeft>
-                <img src={"/img/icon-header-user.png"} alt="" />
+                <img src={"/img/icon-header-user.png"} alt="유저 아이콘" />
               </Header.HamburgerBtnLeft>
               <Header.HamburgerBtnRight>
-                <img src={"/img/icon-header-hbg.png"} alt="" />
+                <img
+                  src={"/img/icon-header-hbg.png"}
+                  alt="햄버거 메뉴 열기 버튼"
+                />
               </Header.HamburgerBtnRight>
             </Header.HamburgerBtn>
-            <Header.HamburgerMenu>
-              <ul>
-                {HAMBURGER_MENUS.map((el) => (
-                  <li key={el.title}>
-                    <span>{el.title}</span>
-                  </li>
-                ))}
-              </ul>
-            </Header.HamburgerMenu>
+            {props.isActive && (
+              <Header.HamburgerMenu>
+                <ul>
+                  {HAMBURGER_MENUS.map((el) => (
+                    <li key={el.title}>
+                      <span>{el.title}</span>
+                    </li>
+                  ))}
+                </ul>
+              </Header.HamburgerMenu>
+            )}
           </Header.Hamburger>
         </Header.MenuBar>
       </Header.InnerWrap>
