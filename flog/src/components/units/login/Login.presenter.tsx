6480ab190584e26992fdda4aad5a0c1d1d22f062
sejung1997 <ksgve1997814@naver.com>
2022-03-26 23:08:43 +0900

    Design: [#79] 회원가입 페이지 1차 완료

--- flog/src/components/units/login/Login.presenter.tsx
+++ flog/src/components/units/login/Login.presenter.tsx
@@ -3,51 +3,48 @@ import * as Login from "./Login.styles";
 
 export default function LoginUI(props) {
   return (
-    <>
-      <Login.Container>
-        <Login.Bg>
-          <Login.LoginBox>
-            <h3>Flog</h3>
-            <Login.Form onSubmit={props.handleSubmit(props.onclickSubmit)}>
-              <Login.FormLabel>이메일</Login.FormLabel>
-              <OutlinedInput01
-                type="text"
-                // id="email"
-                register={props.register("email")}
-                placeholder="이메일을 입력하세요"
-                error={props.errorMsg.email !== ""}
-              ></OutlinedInput01>
-              <Login.ErrorMsg>{props.errorMsg.email}</Login.ErrorMsg>;
-
-              <Login.FormLabel>비밀번호</Login.FormLabel>
-              <OutlinedInput01
-                type="password"
-                // id="password"
-                register={props.register("password")}
-                placeholder="비밀번호를 입력하세요"
-                error={props.errorMsg.password !== ""}
-              ></OutlinedInput01>
-              <Login.ErrorMsg>{props.errorMsg.password}</Login.ErrorMsg>
-              <Login.checkBox>
-                <input type="checkbox"></input>
-                <div>로그인 유지하기</div>
-              </Login.checkBox>
-              <Login.SubmitBtn type="submit">로그인</Login.SubmitBtn>
-              <Login.SubmitBtn
-                className="google"
-                type="button"
-                onClick={props.onclickSubmit}
-              >
-                <img src="/img/login-google.png" /> Google로 시작하기
-              </Login.SubmitBtn>
-            </Login.Form>
-            <Login.SignUp>
-              계정이 아직 없으신가요?
-              <Login.MoveSignUp>회원가입하기</Login.MoveSignUp>
-            </Login.SignUp>
-          </Login.LoginBox>
-        </Login.Bg>
-      </Login.Container>
-    </>
+    <Login.Container>
+      <Login.Bg>
+        <Login.LoginBox>
+          <h3>Flog</h3>
+          <Login.Form onSubmit={props.handleSubmit(props.onclickSubmit)}>
+            <Login.FormLabel>이메일</Login.FormLabel>
+            <OutlinedInput01
+              type="text"
+              // id="email"
+              register={props.register("email")}
+              placeholder="이메일을 입력하세요"
+              error={props.errorMsg.email !== ""}
+            ></OutlinedInput01>
+            <Login.ErrorMsg>{props.errorMsg.email}</Login.ErrorMsg>;
+            <Login.FormLabel>비밀번호</Login.FormLabel>
+            <OutlinedInput01
+              type="password"
+              // id="password"
+              register={props.register("password")}
+              placeholder="비밀번호를 입력하세요"
+              error={props.errorMsg.password !== ""}
+            ></OutlinedInput01>
+            <Login.ErrorMsg>{props.errorMsg.password}</Login.ErrorMsg>
+            <Login.checkBox>
+              <input type="checkbox"></input>
+              <div>로그인 유지하기</div>
+            </Login.checkBox>
+            <Login.SubmitBtn type="submit">로그인</Login.SubmitBtn>
+            <Login.SubmitBtn
+              className="google"
+              type="button"
+              onClick={props.onclickSubmit}
+            >
+              <img src="/img/login-google.png" /> Google로 시작하기
+            </Login.SubmitBtn>
+          </Login.Form>
+          <Login.SignUp>
+            계정이 아직 없으신가요?
+            <Login.MoveSignUp>회원가입하기</Login.MoveSignUp>
+          </Login.SignUp>
+        </Login.LoginBox>
+      </Login.Bg>
+    </Login.Container>
   );
 }
---
+++ flog/src/components/units/signUp/SignUp.container.tsx
@@ -0,0 +1,83 @@
+import SignupUI from "./SignUp.presenter";
+
+import { useForm } from "react-hook-form";
+import { useMutation } from "@apollo/client";
+import { useState } from "react";
+import * as yup from "yup";
+
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import { CREATE_USER } from "./SignUp.queries";
+
+const schema = yup.object().shape({
+  email: yup
+    .string()
+    .required("이메일은 필수 입력 사항입니다.")
+    .email("이메일 형식이 적합하지 않습니다."),
+  password: yup
+    .string()
+    .required("비밀번호는 필수 입력 사항입니다")
+    .matches(
+      /^(?=.*?[a-zA-Z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/,
+      "영문자, 숫자, 특수문자 조합으로 8 ~ 15자리로 설정해 주세요."
+    ),
+
+  password2: yup.string().equals([yup.ref("password")], "비밀번호가 다릅니다."),
+  name: yup.string().required("이름은 필수 입력 사항입니다"),
+});
+interface FormValues {
+  email: string;
+  password: string;
+  password2: string;
+  name: string;
+}
+export default function Signup() {
+  const [pwdType, setPwdType] = useState(false);
+  const [createUser] = useMutation(CREATE_USER);
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onChange",
+  });
+
+  const onclickSubmit = async (data: FormValues) => {
+    console.log(data);
+    try {
+      const result = await createUser({
+        variables: {
+          createUserInput: {
+            nickName: data.name,
+            email: data.email,
+            password: data.password,
+            mainCategoryId: "asdf",
+          },
+        },
+      });
+      console.log(result);
+      alert("회원가입이 완료되었습니다");
+    } catch (error) {
+      if (error instanceof Error) {
+        // if (error.message.includes("이메일")) {
+        //   setErrorMsg({ ...errorMsg, email: error.message });
+        //   resetError(data.email, "email");
+        // }
+        // if (error.message.includes("비밀번호")) {
+        //   setErrorMsg({ ...errorMsg, password: error.message });
+        //   resetError(data.email, "password");
+        // }
+      }
+    }
+  };
+  const pwdToggle = () => {
+    setPwdType((prev) => !prev);
+  };
+  return (
+    <SignupUI
+      register={register}
+      handleSubmit={handleSubmit}
+      onclickSubmit={onclickSubmit}
+      formState={formState}
+      pwdType={pwdType}
+      pwdToggle={pwdToggle}
+    />
+  );
+}
