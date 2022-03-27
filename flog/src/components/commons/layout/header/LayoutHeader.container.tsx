806d82944808cdab56f02ef7a43cedadf2cdbfb4
Jooeun-K <90050960+Jooeun-K@users.noreply.github.com>
2022-03-27 23:16:32 +0900

    Feat: [#91] 레이아웃 컴포넌트 라우터 연결

--- flog/src/components/commons/layout/header/LayoutHeader.container.tsx
+++ flog/src/components/commons/layout/header/LayoutHeader.container.tsx
@@ -1,20 +1,36 @@
+import { useRouter } from "next/router";
 import { useState } from "react";
 import LayoutHeaderUI from "./LayoutHeader.presenter";
 
 export default function LayoutHeader() {
+  const router = useRouter();
   const [isActive, setIsActive] = useState(false);
+  const [isLogIn, setIsLogIn] = useState(false);
+
   const onClickLogo = () => {
-    alert("Logo 클릭 시 작동하는 함수");
+    router.push("/main");
   };
   const onClickHamburgerBtn = () => {
     setIsActive((prev) => !prev);
   };
+  const onClickMoveToHamburger = (url: string) => () => {
+    router.push(url);
+    setIsActive((prev) => !prev);
+  };
+
+  // 빨간 줄 없애기 위한 임시 함수 (추후 로그인 구현되면 삭제 예정)
+  const temp = () => {
+    setIsLogIn((prev) => !prev);
+  };
+  console.log(temp);
 
   return (
     <LayoutHeaderUI
       onClickLogo={onClickLogo}
       onClickHamburgerBtn={onClickHamburgerBtn}
+      onClickMoveToHamburger={onClickMoveToHamburger}
       isActive={isActive}
+      isLogIn={isLogIn}
     />
   );
 }
--- flog/src/components/commons/layout/header/LayoutHeader.types.ts
+++ flog/src/components/commons/layout/header/LayoutHeader.types.ts
@@ -1,5 +1,7 @@
 export interface ILayoutHeaderUIProps {
   onClickLogo: () => void;
   onClickHamburgerBtn: () => void;
+  onClickMoveToHamburger: (url: string) => () => void;
   isActive: boolean;
+  isLogIn: boolean;
 }
