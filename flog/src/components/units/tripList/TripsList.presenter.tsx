ed381730af8d04352cead5d4db63019f1464ba91
wisdoom03 <wisdoom03@gmail.com>
2022-03-28 10:22:47 +0900

    Feat: [#95] 여행 리스트 불러오기, 검색 미완료

--- flog/src/components/units/tripList/TripsList.presenter.tsx
+++ flog/src/components/units/tripList/TripsList.presenter.tsx
@@ -2,25 +2,19 @@ import Dropdown01 from "../../commons/dropdowns/01/Dropdown01.container";
 import * as List from "./TripsList.styles";
 
 export default function TripListUI(props) {
-  const arr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-
+  console.log(props.shareData);
   return (
     <List.List>
       <List.Head>
         <List.Label>{props.isMine ? "지난 여행" : "여행 족보"}</List.Label>
         <Dropdown01 />
-        {/* <List.Select>
-          <List.SelectOption selected>최신순</List.SelectOption>
-          <List.SelectOption>조회순</List.SelectOption>
-          <List.SelectOption>과거순</List.SelectOption>
-        </List.Select> */}
       </List.Head>
       <List.TripList>
-        {arr.map((_el, index) => (
+        {props.shareData?.fetchShareSchedules.map((el, index) => (
           <>
-            <List.CardWrapper key={index}>
+            <List.CardWrapper key={el.id}>
               <List.Wrapper>
-                {index === 2 && (
+                {props.isMine && (
                   <List.Mark>
                     <img src="/img/icon-body-marker.svg" alt="공유한 여행" />
                   </List.Mark>
@@ -30,7 +24,6 @@ export default function TripListUI(props) {
                     <img src="/img/icon-body-edit.svg" alt="여행이미지 수정" />
                   </List.EditWrap>
                 )}
-
                 <List.Image>
                   <img
                     src={`/img/trips/user/trip-img${index + 1}.jpg`}
@@ -38,10 +31,11 @@ export default function TripListUI(props) {
                   />
                 </List.Image>
                 <List.Text>
-                  <List.Title>엄마와 함께한 제주도여행</List.Title>
+                  <List.Title>{el.title}</List.Title>
                   <List.Subtitle>
-                    <List.Date>22.02.03 ~ 22.02.04</List.Date>
-                    <List.Region>| 전라남도 전주</List.Region>
+                    <List.Date>{el.startDate} ~ </List.Date>
+                    <List.Date>{el.endDate}</List.Date>
+                    <List.Region>| {el.location}</List.Region>
                   </List.Subtitle>
                 </List.Text>
               </List.Wrapper>
