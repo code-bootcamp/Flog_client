126af86f6d0bac5888ab7da61dadced1eb3c839e
sejung1997 <ksgve1997814@naver.com>
2022-03-28 21:32:26 +0900

    [Feat] 로그 작성/상세 페이지 API 연결 시도

--- flog/src/components/units/tripWrite/log/list/TripWriteLogList.container.tsx
+++ flog/src/components/units/tripWrite/log/list/TripWriteLogList.container.tsx
@@ -1,5 +1,7 @@
-import { useMemo, useRef, useState } from "react";
+import { useQuery } from "@apollo/client";
+import { useEffect, useMemo, useRef, useState } from "react";
 import TripWriteLogListUI from "./TripWriteLogList.presenter";
+import { FETCH_DETAIL_SCHEDULES } from "./TripWriteLogList.queries";
 const schedules = [
   [
     { name: "해운대 암소갈비집1", des: "존맛탱! 로컬 맛집 인정" },
@@ -26,7 +28,56 @@ const schedules = [
 ];
 export default function TripWriteLogList(props) {
   const ArrayLength = schedules.length;
+
   const [isShow, setIsShow] = useState(new Array(ArrayLength).fill(false));
+  const { data } = useQuery(FETCH_DETAIL_SCHEDULES, {
+    variables: {
+      scheduleId: "43e52c82-16d9-4e1d-af30-8c1a4cd572fc",
+      userId: "ac7ac614-7afb-4e0c-a3e4-afcd3c604d73",
+    },
+  });
+  // const { data: schedule1 } = useQuery(FETCH_DETAIL_SCHEDULE, {
+  //   variables: {
+  //     scheduleId: "43e52c82-16d9-4e1d-af30-8c1a4cd572fc",
+  //     userId: "ac7ac614-7afb-4e0c-a3e4-afcd3c604d73",
+  //     day: "1",
+  //   },
+  // });
+  // const { data: schedule2 } = useQuery(FETCH_DETAIL_SCHEDULE, {
+  //   variables: {
+  //     scheduleId: "43e52c82-16d9-4e1d-af30-8c1a4cd572fc",
+  //     userId: "ac7ac614-7afb-4e0c-a3e4-afcd3c604d73",
+  //     day: "2",
+  //   },
+  // });
+  // const { data: schedule3 } = useQuery(FETCH_DETAIL_SCHEDULE, {
+  //   variables: {
+  //     scheduleId: "43e52c82-16d9-4e1d-af30-8c1a4cd572fc",
+  //     userId: "ac7ac614-7afb-4e0c-a3e4-afcd3c604d73",
+  //     day: "3",
+  //   },
+  // });
+
+  // useEffect(() => {
+  //   schedules[0] = schedule1?.fetchDetailSchedule;
+  //   schedules[1] = schedule2?.fetchDetailSchedule;
+  //   schedules[2] = schedule3?.fetchDetailSchedule;
+  //   console.log(schedule1);
+  // }, [schedule1, schedule2, schedule3]);
+
+  // useEffect(() => {
+  //   console.log(schedule1);
+  // }, [schedule1]);
+  useEffect(() => {
+    if (!data) return;
+    console.log(schedules);
+  }, [schedules]);
+
+  // data?.fetchDetailSchedules?.forEach((el, index) => {
+  //   if (el.day === "1") return schedules[0].push(el);
+  //   if (el.day === "2") return schedules[1].push(el);
+  //   if (el.day === "3") return schedules[2].push(el);
+  // });
 
   const allRef = schedules.map((x) =>
     x.map((el) => useRef<HTMLLinkElement>(null))
@@ -34,6 +85,7 @@ export default function TripWriteLogList(props) {
 
   const addElement = (dayIndex, elIndex) => () => {
     allRef[dayIndex][elIndex].current?.click();
+    console.log(data);
   };
   const toggle = (index) => () => {
     const temp = new Array(ArrayLength).fill(false);
@@ -51,6 +103,7 @@ export default function TripWriteLogList(props) {
     });
   };
   const isMine = false;
+
   return (
     <TripWriteLogListUI
       schedules={schedules}
