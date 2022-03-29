deef8c01c0741ac2a5bfcc162393fda8bfe91022
sejung1997 <ksgve1997814@naver.com>
2022-03-29 10:39:42 +0900

    Design: [#126] 리프레쉬토큰과 https

--- flog/pages/_app.tsx
+++ flog/pages/_app.tsx
@@ -17,6 +17,8 @@ import {
   useEffect,
 } from "react";
 import { createUploadLink } from "apollo-upload-client";
+import { getAccessToken } from "../src/components/commons/libraries/getAccessToken";
+import { onError } from "@apollo/client/link/error";
 
 interface IGlobalContext {
   accessToken?: string;
@@ -34,15 +36,47 @@ function MyApp({ Component, pageProps }: AppProps) {
   useEffect(() => {
     if (localStorage.getItem("accessToken"))
       setAccessToken(localStorage.getItem("accessToken") || "{}");
+    getAccessToken().then((newAccessToken) => {
+      setAccessToken(newAccessToken);
+    });
   }, []);
+
+  const errorLink = onError(({ graphQLErrors, operation, forward }) => {
+    // 1. 에러를 캐치
+    if (graphQLErrors) {
+      for (const err of graphQLErrors) {
+        // 2. 해당 에러가 토큰만료 에러인지 체크(UNAUTHENTICATED)
+        if (err.extensions.code === "UNAUTHENTICATED") {
+          // 3. refreshToken으로 accessToken을 재발급 받기
+          getAccessToken().then((newAccessToken) => {
+            // 4. 재발급 받은 accessToken 저장하기
+            setAccessToken(newAccessToken);
+
+            // 5. 재발급 받은 accessToken으로 방금 실패한 쿼리 재요청하기
+            // operation에 실패한 쿼리들 들어가있음
+            operation.setContext({
+              headers: {
+                ...operation.getContext().headers,
+                Authorization: `Bearer ${newAccessToken}`,
+              },
+            }); // 설정 변경(accessToken만!! 바꿔치기)
+            return forward(operation); // 변경된 operation 재요청하기!!
+          });
+        }
+      }
+    }
+  });
+
   const uploadLink = createUploadLink({
-    uri: "http://34.64.249.11:3000/graphql",
+    uri: "https://gyeoriii.shop/graphql",
     headers: { Authorization: `Bearer ${accessToken}` },
+    credentials: "include",
   });
   const client = new ApolloClient({
-    link: ApolloLink.from([uploadLink as unknown as ApolloLink]),
+    link: ApolloLink.from([errorLink, uploadLink as unknown as ApolloLink]),
     cache: new InMemoryCache(),
   });
+
   return (
     <GlobalContext.Provider value={value}>
       <ApolloProvider client={client}>
